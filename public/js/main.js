2d42fef10ceb92670f0be0eea5c6ea4719ab6108
tlc2880 <tommylcao@gmail.com>
2019-11-23 06:58:43 -0800

    Added pagination.

--- public/js/main.js
+++ public/js/main.js
@@ -20,3 +20,22 @@ $(document).ready(function(){
     });
   });
 });
+
+// Pagination
+var items = $(".list-wrapper .list-item");
+var numItems = items.length;
+var perPage = 3;
+
+items.slice(perPage).hide();
+
+$('#pagination-container').pagination({
+  items: numItems,
+  itemsOnPage: perPage,
+  prevText: "&laquo;",
+  nextText: "&raquo;",
+  onPageClick: function (pageNumber) {
+    var showFrom = perPage * (pageNumber - 1);
+    var showTo = showFrom + perPage;
+    items.hide().slice(showFrom, showTo).show();
+  }
+});
